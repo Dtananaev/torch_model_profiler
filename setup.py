68915c615e3dcc8a62b8bd54772b63fb7a091484
Denis Tananaev <d.d.tananaev@gmail.com>
2025-06-20 13:56:04 +0200

    added first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+from torchinspect import __version__
+
+setup(
+    name='torchinspect',
+    version=__version__,
+    packages=find_packages(exclude=['examples']),
+    install_requires=[
+        'numpy>=1.14',
+        'torch>=1.4',
+        'torchvision>=0.4',
+    ],
+    url='',
+    license='MIT',
+)
